a88b231b96cdb8f7460ce11a7a0117a47624459e
LinZC2097 <linzhichao_2813@163.com>
2020-02-10 18:26:47 -0500

    Project 1 A* 0.1 failure

--- Project_1.py
+++ Project_1.py
@@ -1,4 +1,6 @@
 import copy
+import heapq
+from math import sqrt, fabs
 
 
 class ShortestPath:
@@ -33,6 +35,8 @@ class ShortestPath:
                 vertex.append(int(i))
             self.cost_mat[vertex[0]][vertex[1]] = vertex[2]
             self.cost_mat[vertex[1]][vertex[0]] = vertex[2]
+        for i in range(self.size):
+            self.cost_mat[i][i] = 0
 
     def create_square_mat(self) -> list:
         reader = self.read_file(self.file_v_adds)
@@ -46,9 +50,9 @@ class ShortestPath:
                 for j in range(self.size):
                     self.apsp_mat[i][j] = min(self.apsp_mat[i][k] + self.apsp_mat[k][j], self.apsp_mat[i][j])
 
-    def shortest_path_apsp(self, i: int, j: int) -> int:
+    def shortest_path_apsp(self, start: int, end: int) -> int:
         self.apsp()
-        return self.apsp_mat[i][j]
+        return self.apsp_mat[start][end]
 
     def create_adjacency_list(self) -> list:
         reader = self.read_file(self.file_e_adds)
@@ -59,25 +63,68 @@ class ShortestPath:
 
             self.adjacency_list[vertex[0]][vertex[1]] = vertex[2]
             self.adjacency_list[vertex[1]][vertex[0]] = vertex[2]
-        for i in range(len(self.adjacency_list)):
-            print(i, self.adjacency_list[i])
 
-    def shortest_path_astar(self, i: int, j: int) -> int:
+    def astar_hcost_function(self, current: int, end: int) -> float:
+        self.create_square_mat()
+        if self.square_mat[current] == self.square_mat[end]:
+            return 14.1
+        # elif fabs(self.square_mat[current] % 10 - self.square_mat[end] % 10) == 1 \
+        #         and (self.square_mat[current] // 10 == self.square_mat[end] // 10
+        #         or fabs(self.square_mat[current] // 10 - self.square_mat[end] // 10) == 1):
+        #     return 0
+        else:
+            height = fabs(self.square_mat[current] // 10 - self.square_mat[end] // 10) - 1
+            weight = fabs(self.square_mat[current] % 10 - self.square_mat[end] % 10) - 1
+            a = "%.2f" % sqrt((height * 10) ** 2 + (weight * 10) ** 2)
 
+            return float(a)
 
+    def shortest_path_astar(self, start: int, end: int) -> int:
+        discovery = [None for _ in range(self.size)]
+        discovery[start] = 0
+        previous = [None for _ in range(self.size)]
 
+        previous[start] = start
+        frontier_heap = [(0, start)]
+        next_vertex = heapq.heappop(frontier_heap)
 
-        pass
+        while next_vertex[1] != end:
+
+            for val in self.adjacency_list[next_vertex[1]]:
+                if discovery[val] is None:
+                    discovery[val] = discovery[next_vertex[1]] + self.adjacency_list[next_vertex[1]][val]
+                    previous[val] = next_vertex[1]
+                    heapq.heappush(frontier_heap, (self.astar_hcost_function(val, end) + discovery[val], val))
+            next_vertex = heapq.heappop(frontier_heap)
+
+        return discovery[previous[next_vertex[1]]] + self.adjacency_list[previous[next_vertex[1]]][next_vertex[1]]
 
 
 if __name__ == '__main__':
     print("hello world")
+    print("------------------------")
     test = ShortestPath(100,
-                        "/Users/marsscho/Desktop/6511 Artificial Intelligence/Project/Project 1/graphs/graph100_0.2/v.txt",
-                        "/Users/marsscho/Desktop/6511 Artificial Intelligence/Project/Project 1/graphs/graph100_0.2/e.txt")
+                        "/Users/marsscho/Desktop/6511 Artificial Intelligence/Project/Project 1/graphs/graph100_0.1/v.txt",
+                        "/Users/marsscho/Desktop/6511 Artificial Intelligence/Project/Project 1/graphs/graph100_0.1/e.txt")
 
     test.create_cost_mat()
     test.create_square_mat()
     test.create_adjacency_list()
-    print(test.shortest_path_apsp(1, 1))
+    # print(test.astar_hcost_function(20, 1))
+    start = 1
+    i = 0
+    for end in range(2, 100):
+        apsp = test.shortest_path_apsp(start, end)
+        astar = test.shortest_path_astar(start, end)
+        if apsp == astar:
+            print(end, apsp, astar)
+        else:
+            print(end, apsp, astar, "!!!!!!!!!")
+            i += 1
+    print(i)
+    # end = 5
+    # print(test.shortest_path_apsp(start, end))
+    # print(test.shortest_path_astar(start, end))
+
+
 
